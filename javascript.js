f1050ce6af78294842447368c9cde61f5c099e1d
Patrick Collins <1patrick.collin1@gmail.com>
2025-10-02 16:35:33 +1300

    begin word-randomiser.js

--- javascript.js
+++ javascript.js
@@ -6,12 +6,11 @@ console.log('hi')
 
 let color1 = new RGB(255, 126, 0)
 let color2 = new RGB(0, 126, 126)
-changeNameColor(nameElement, color1, color2, 10)
+changeElementColor(nameElement, color1, color2, 10)
 
-function changeNameColor(element, color1, color2, time) {
-  // console.log(color1.getColorString())
+function changeElementColor(element, color1, color2, time) {
   color1.changeValues()
   color2.changeValues()
   nameElement.style.backgroundImage = `linear-gradient(to right, rgb(${color1.getColorString()}), rgb(${color2.getColorString()})`
-  setTimeout(changeNameColor, time, element, color1, color2, time)
+  setTimeout(changeElementColor, time, element, color1, color2, time)
 }
